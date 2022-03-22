c1380d1e8e1cd64b64f5698252ac9552c091c31b
rage875 <rage.875@gmail.com>
2022-03-22 16:59:31 -0600

    Counting and searching ToDos

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,20 +7,46 @@ import { TodoCreateButtom } from './TodoCreateButtom';
 
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { id: 1, text: 'Tarea 1', completed: true},
   { id: 2, text: 'Tarea 2', completed: false},
-  { id: 3, text: 'Tarea 3', completed: false},
+  { id: 3, text: 'Tarea 3', completed: true},
 ]
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter( todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText);
+    })
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter
+        total = {totalTodos}
+        completed = {completedTodos}
+      />
+
+      <TodoSearch
+        searchValue = {searchValue}
+        setSearchValue = {setSearchValue}
+      />
 
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
--- react-app/src/TodoSearch.js
+++ react-app/src/TodoSearch.js
@@ -1,13 +1,11 @@
 import React from "react";
 import './TodoSearch.css';
 
-function TodoSearch() {
-    const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({searchValue, setSearchValue}) {
 
     const onSearchValueChange = (event) => {
         setSearchValue(event.target.value);
     };
-
 
     return (
         <input className = "TodoSearch" placeholder="Default text"
