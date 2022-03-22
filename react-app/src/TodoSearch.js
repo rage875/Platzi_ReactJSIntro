2b15517c30e3f0f7ceb6599803301f48a7b69dc6
rage875 <rage.875@gmail.com>
2022-03-22 16:21:24 -0600

    State handler

--- react-app/src/TodoSearch.js
+++ react-app/src/TodoSearch.js
@@ -2,14 +2,17 @@ import React from "react";
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
 
     return (
         <input className = "TodoSearch" placeholder="Default text"
-            onChange={onSearchValueChange}
+            value = {searchValue}
+            onChange = {onSearchValueChange}
         />
     )
 }
